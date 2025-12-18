1ced5f48820252aff5eea86d09761816d463d360
Rasmus <nordalrasmus01@gmail.com>
2025-12-18 18:04:59 +0100

    day 4 done

--- 4/t1.py
+++ 4/t1.py
@@ -5,4 +5,44 @@ else:
     s = "input.txt"
 with open(s) as f:
     lines = list(l.strip() for l in f.readlines())
+R = len(lines)
+C = len(lines[0])
 
+def adj(r, c):
+    coords = []
+    if r != 0:
+        coords.append((r-1, c))
+        if c != 0:
+            coords.append((r-1, c-1))
+        if c != C-1:
+            coords.append((r-1, c+1))
+    if r != R-1:
+        coords.append((r+1, c))
+        if c != 0:
+            coords.append((r+1, c-1))
+        if c != C-1:
+            coords.append((r+1, c+1))
+    if c != 0:
+        coords.append((r, c-1))
+    if c != C-1:
+        coords.append((r, c+1))
+    return coords
+
+key = 0
+for r, line in enumerate(lines):
+    for c, val in enumerate(line):
+        if val == ".":
+            print(".", end="")
+            continue
+        count = 0
+        for cr, cc in adj(r, c):
+            if lines[cr][cc] == "@":
+                if count == 3:
+                    print("@", end="")
+                    break
+                count += 1
+        else:
+            print("x", end="")
+            key += 1
+    print()
+print(key)
--- 4/t2.py
+++ 4/t2.py
@@ -4,5 +4,50 @@ if len(argv) > 1 and argv[1].strip() in ("test", "t"):
 else:
     s = "input.txt"
 with open(s) as f:
-    lines = list(l.strip() for l in f.readlines())
+    lines = list(bytearray(l.strip().encode()) for l in f.readlines())
+R = len(lines)
+C = len(lines[0])
 
+def adj(r, c):
+    coords = []
+    if r != 0:
+        coords.append((r-1, c))
+        if c != 0:
+            coords.append((r-1, c-1))
+        if c != C-1:
+            coords.append((r-1, c+1))
+    if r != R-1:
+        coords.append((r+1, c))
+        if c != 0:
+            coords.append((r+1, c-1))
+        if c != C-1:
+            coords.append((r+1, c+1))
+    if c != 0:
+        coords.append((r, c-1))
+    if c != C-1:
+        coords.append((r, c+1))
+    return coords
+
+key = 0
+repeat = True
+while repeat:
+    repeat = False
+    for r,  line in enumerate(lines):
+        for c, val in enumerate(line):
+            if val == ord("."):
+                print(".", end="")
+                continue
+            count = 0
+            for cr, cc in adj(r, c):
+                if lines[cr][cc] == ord(b"@"):
+                    if count == 3:
+                        print("@", end="")
+                        break
+                    count += 1
+            else:
+                print("x", end="")
+                lines[r][c] = ord(".")
+                repeat=True
+                key += 1
+        print()
+print(key)
